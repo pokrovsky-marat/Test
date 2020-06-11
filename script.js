1c42edffa565f2cd8f186031650f266b1cc92df6
Marat Isaev <savant.87@mail.ru>
2020-06-11 22:44:16 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function expandedForm(num) {
+  // Your code here
+}
+expandedForm(12); // Should return '10 + 2'
+expandedForm(42); // Should return '40 + 2'
+expandedForm(70304); // Should return '70000 + 300 + 4'
