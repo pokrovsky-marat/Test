206c9b4d70a307ab3a406cc3ff21374f03d30ab0
Marat Isaev <savant.87@mail.ru>
2020-06-12 21:22:22 +0600

    Solution for task about array

--- script.js
+++ script.js
@@ -1,6 +1,71 @@
-function expandedForm(num) {
-  // Your code here
-}
-expandedForm(12); // Should return '10 + 2'
-expandedForm(42); // Should return '40 + 2'
-expandedForm(70304); // Should return '70000 + 300 + 4'
+
+
+snail = function (array) {
+  let resultArray = [];
+  let n = array[0].length;
+  let max = n;
+  let min = 0;
+  let state = "right";
+  if (n == 0) {
+    return resultArray;
+  }
+  for (let i = 0, k = 0, j = -1; i < n * n; i++) {
+    if (state == "right") {
+      j++;
+      if (j < max) {
+        resultArray.push(array[k][j]);
+      } else {
+        j--;
+        state = "down";
+        i--;
+      }
+    } else if (state == "down") {
+      k++;
+      if (k < max) {
+        resultArray.push(array[k][j]);
+      } else {
+        k--;
+        state = "left";
+        i--;
+      }
+    } else if (state == "left") {
+      j--;
+
+      if (j >= min) {
+        resultArray.push(array[k][j]);
+      } else {
+        j++;
+        state = "up";
+        i--;
+        max--;
+        min++;
+      }
+    } else if (state == "up") {
+      k--;
+      if (k >= min) {
+        resultArray.push(array[k][j]);
+      } else {
+        k++;
+        state = "right";
+        i--;
+      }
+    }
+  }
+
+  return resultArray;
+};
+
+console.log(
+  snail([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ])
+);
+snail([
+  [1, 2, 3, 4, 5],
+  [6, 7, 8, 9, 10],
+  [11, 12, 13, 14, 15],
+  [16, 17, 18, 19, 20],
+  [21, 22, 23, 24, 25],
+]);
