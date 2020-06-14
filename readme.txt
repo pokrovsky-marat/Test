9b350864938095cbb8cd68077827f2c12ed9a8e8
Marat Isaev <savant.87@mail.ru>
2020-06-14 11:27:43 +0600

    I solved one very difficult task from codewars

--- readme.txt
+++ readme.txt
@@ -1,1 +1,39 @@
-This is simple text file. Nothing exciting about it.
+function nextBigger(n) {
+  function factorial(n) {
+    return n ? n * factorial(n - 1) : 1;
+  }
+  let digit = n + "";
+  let cntIteration = factorial(digit.length);
+  let max = digit.length - 1;
+  let temp;
+  digit = digit.split("");
+  let listCombination = [];
+  cntIteration = cntIteration / max;
+
+  for (let i = 0; i < cntIteration; i++) {
+    for (let j = max; j > 0; j--) {
+      temp = digit[j - 1];
+      digit[j - 1] = digit[j];
+      digit[j] = temp;
+      listCombination.push(digit.join(""));
+    }
+  }
+
+  let setList = new Set(listCombination);
+  listCombination = Array.from(setList);
+  mappedList = listCombination.filter((item) => item[0] !== "0");
+  listCombination = mappedList;
+
+  listCombination.sort((a, b) => a - b);
+  console.log(listCombination);
+  let index = listCombination.indexOf(n + "");
+  return index < listCombination.length - 1
+    ? listCombination[index + 1] * 1
+    : -1;
+}
+console.log(nextBigger(1234567890));
+// Test.assertEquals(nextBigger(12),21)
+// Test.assertEquals(nextBigger(513),531)
+// Test.assertEquals(nextBigger(2017),2071)
+// Test.assertEquals(nextBigger(414),441)
+// Test.assertEquals(nextBigger(144),414)
--- script.js
+++ script.js
@@ -1,71 +1,20 @@
+function nextBigger(n) {
+  let max = (n + '').split('').sort((a, b) => b - a).join('');
+  if (n == max) return -1;
+  do n++;
+  while (max != (n + '').split('').sort((a, b) => b - a).join(''));
+  return n;
+}
+console.log(nextBigger(9876543210 ));
+console.log(nextBigger(59884848459853));
+console.log(nextBigger(513 ));
+console.log(nextBigger(2017 ));
+console.log(nextBigger(414 ));
+console.log(nextBigger(144 ));
 
 
-snail = function (array) {
-  let resultArray = [];
-  let n = array[0].length;
-  let max = n;
-  let min = 0;
-  let state = "right";
-  if (n == 0) {
-    return resultArray;
-  }
-  for (let i = 0, k = 0, j = -1; i < n * n; i++) {
-    if (state == "right") {
-      j++;
-      if (j < max) {
-        resultArray.push(array[k][j]);
-      } else {
-        j--;
-        state = "down";
-        i--;
-      }
-    } else if (state == "down") {
-      k++;
-      if (k < max) {
-        resultArray.push(array[k][j]);
-      } else {
-        k--;
-        state = "left";
-        i--;
-      }
-    } else if (state == "left") {
-      j--;
-
-      if (j >= min) {
-        resultArray.push(array[k][j]);
-      } else {
-        j++;
-        state = "up";
-        i--;
-        max--;
-        min++;
-      }
-    } else if (state == "up") {
-      k--;
-      if (k >= min) {
-        resultArray.push(array[k][j]);
-      } else {
-        k++;
-        state = "right";
-        i--;
-      }
-    }
-  }
-
-  return resultArray;
-};
-
-console.log(
-  snail([
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-  ])
-);
-snail([
-  [1, 2, 3, 4, 5],
-  [6, 7, 8, 9, 10],
-  [11, 12, 13, 14, 15],
-  [16, 17, 18, 19, 20],
-  [21, 22, 23, 24, 25],
-]);
+// Test.assertEquals(nextBigger(12),21)
+// Test.assertEquals(nextBigger(513),531)
+// Test.assertEquals(nextBigger(2017),2071)
+// Test.assertEquals(nextBigger(414),441)
+// Test.assertEquals(nextBigger(144),414)
